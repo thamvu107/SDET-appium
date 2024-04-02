5180f279b9224fe48f1da8b4edb358f5c586f8b1
Tham <thamvu107@gmail.com>
2024-04-02 09:14:24 +0700

    init appium repo

---
+++ src/test/java/driver/MobileCapabilityType.java
@@ -0,0 +1,8 @@
+package driver;
+
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class MobileCapabilityType extends UiAutomator2Options {
+
+    public static final String PLATFORM_NAME = "platformName";
+}
---
+++ src/test/java/driver/Platform.java
@@ -0,0 +1,6 @@
+package driver;
+
+public enum Platform {
+    ANDROID,
+    IOS
+}
