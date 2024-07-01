12458a0cf423a8ed9f6da36ae4294b7dbb0756cb
Tham <thamvu107@gmail.com>
2024-07-01 09:54:18 +0700

    inscrease timeout to create driver

--- src/main/java/constants/android/IAndroidDriverSetting.java
+++ src/main/java/constants/android/IAndroidDriverSetting.java
@@ -7,9 +7,9 @@ import static java.time.Duration.ofMillis;
 public interface AndroidDriverSetting {
     boolean APP_WAIT_FOR_LAUNCH = true;
     Duration APP_WAIT_FOR_LAUNCH_TIME = Duration.ofMillis(180_000L);
-    Duration UIAUTOMATOR2_SERVER_LAUNCH_TIMEOUT = ofMillis(180_0000L);
-    Duration UIAUTOMATOR2_SERVER_INSTALL_TIMEOUT = ofMillis(180_000L);
-    Duration UIAUTOMATOR2_SERVER_READY_TIMEOUT = ofMillis(180_000L);
+    Duration UIAUTOMATOR2_SERVER_LAUNCH_TIMEOUT = ofMillis(240_0000L);
+    Duration UIAUTOMATOR2_SERVER_INSTALL_TIMEOUT = ofMillis(2400_000L);
+    Duration UIAUTOMATOR2_SERVER_READY_TIMEOUT = ofMillis(240_000L);
 
     Duration NEW_COMMAND_TIMEOUT = ofMillis(240_000L);
 }
---
+++ src/test/java/learning/LaunchingEmulator.java
@@ -0,0 +1,2 @@
+package learning;public class EmulatorStarting {
+}
